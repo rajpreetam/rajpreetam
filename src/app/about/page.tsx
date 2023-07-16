123845ed315c0896adcd5458cc7d8c498003368b
Preetam Raj <dev.preetamraj@gmail.com>
2023-07-17 04:26:01 +0530

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,20 @@
 import Link from "next/link";
 
+const skillsData = [
+    {
+        skill: 'Languages:',
+        items: ['Python', 'C++', 'Javascript', 'Java']
+    },
+    {
+        skill: 'Frameworks/Web/App Technologies:',
+        items: ['Django', 'Django Rest Framework', 'FastAPI', 'ReactJS', 'NextJS', 'Redux Toolkit', 'HTML5', 'CSS3', 'Tailwind CSS', 'Material UI', 'Bootstrap']
+    },
+    {
+        skill: 'Familiar Technologies:',
+        items: ['MySQL', 'Postgresql', 'SQLAlchemy', 'AWS EC2', 'AWS RDS', 'AWS S3', 'GIT', 'Github', 'Socket Programming', 'Pandas', 'Numpy', 'Mathplotlib']
+    },
+];
+
 const AboutPage = () => {
     return (
         <div className='min-h-[calc(100vh-182px)] w-[calc(100vw-32px)] sm:w-full flex flex-col space-y-4'>
@@ -8,53 +23,21 @@ const AboutPage = () => {
                     <h1 className="text-2xl font-semibold tracking-wide">Who Am I?</h1>
                     <button className="text-purple-500 text-2xl underline underline-offset-4">Resume</button>
                 </div>
-                <p className="font-light tracking-wider text-justify">Hi, my name is <span className="font-semibold text-purple-500">Preetam Raj</span>. I am from Patna, Bihar. I am graduated from IIT Mandi with major in Mechanical Engineering and minor in Computer Science and Engineering. I am very much enthusiastic in web and app development and did many <Link href='/portfolio'>projects</Link> in various web/app dev tech stack.</p>
+                <p className="font-light tracking-wider text-justify">Hi, my name is <span className="font-semibold text-purple-500">Preetam Raj</span>. I am from Patna, Bihar. I am graduated from IIT Mandi with major in Mechanical Engineering and minor in Computer Science and Engineering. I am very much enthusiastic in web and app development and did many <Link className='text-purple-500' href='/portfolio'>projects</Link> in various web/app dev tech stack.</p>
             </div>
             <div className="flex flex-col space-y-2">
                 <h1 className="text-2xl font-semibold tracking-wide">Skills</h1>
                 <div className="flex flex-col space-y-4">
-                    <div className="flex flex-col space-y-2">
-                        <h1 className="underline underline-offset-4">Languages:</h1>
-                        <div className="flex flex-wrap items-center">
-                            <span className="font-light mr-4">Python</span>
-                            <span className="font-light mr-4">C++</span>
-                            <span className="font-light mr-4">Javascript</span>
-                            <span className="font-light">Java</span>
+                    {skillsData.map(item => (
+                        <div key={item.skill} className="flex flex-col space-y-2">
+                            <h1 className="underline underline-offset-4">{item.skill}</h1>
+                            <div className="flex flex-wrap items-center">
+                                {item.items.map(skill => (
+                                    <span key={skill} className="font-light mr-4">{skill}</span>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                        <h1 className="underline underline-offset-4">Frameworks/Web/App Technologies:</h1>
-                        <div className="flex flex-wrap items-center">
-                            <span className="font-light mr-4">Django</span>
-                            <span className="font-light mr-4">Django Rest Framework</span>
-                            <span className="font-light mr-4">FastAPI</span>
-                            <span className="font-light mr-4">ReactJS</span>
-                            <span className="font-light mr-4">NextJS</span>
-                            <span className="font-light mr-4">Redux Toolkit</span>
-                            <span className="font-light mr-4">HTML5</span>
-                            <span className="font-light mr-4">CSS3</span>
-                            <span className="font-light mr-4">Tailwind CSS</span>
-                            <span className="font-light mr-4">Material UI</span>
-                            <span className="font-light">Bootstrap</span>
-                        </div>
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                        <h1 className="underline underline-offset-4">Familiar Technologies:</h1>
-                        <div className="flex flex-wrap items-center">
-                            <span className="font-light mr-4">MySQL</span>
-                            <span className="font-light mr-4">Postgresql</span>
-                            <span className="font-light mr-4">SQLAlchemy</span>
-                            <span className="font-light mr-4">AWS EC2</span>
-                            <span className="font-light mr-4">AWS RDS</span>
-                            <span className="font-light mr-4">AWS S3</span>
-                            <span className="font-light mr-4">Git</span>
-                            <span className="font-light mr-4">Github</span>
-                            <span className="font-light mr-4">Socket Programming</span>
-                            <span className="font-light mr-4">Pandas</span>
-                            <span className="font-light mr-4">Numpy</span>
-                            <span className="font-light">Matplotlib</span>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
